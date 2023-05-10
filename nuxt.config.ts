fa5a287a6f6db10ad12c8abff54143d078a55cee
Alex van der Werff <alex.van-der-werff@twobulls.com>
2023-05-10 13:11:14 +1000

    npx inkline init

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,42 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
-export default defineNuxtConfig({
+import { resolve } from 'path';
 
-})
+export default defineNuxtConfig({
+    modules: ['@inkline/plugin/nuxt'],
+    inkline: {
+        /**
+         * @inkline/config
+         * @description provides configuration file specific options
+         */
+
+        configFile: resolve(process.cwd(), 'inkline.config.ts'),
+        extName: '.scss',
+        outputDir: resolve(__dirname, '.inkline/css'),
+
+        /**
+         * @inkline/inkline
+         * @description provides configuration file specific options
+         */
+
+        globals: {
+            color: '',
+            colorMode: 'system',
+            colorModeStrategy: 'localStorage',
+            componentOptions: {},
+            locale: 'en',
+            size: '',
+            validateOn: ['input', 'blur']
+        },
+
+        /**
+         * @inkline/plugin
+         * @description provides import specific options
+         */
+
+        import: {
+            mode: 'auto',
+            scripts: true,
+            styles: true,
+            utilities: true
+        }
+    }
+});
